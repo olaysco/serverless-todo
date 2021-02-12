107f1df3356020aaca8fc8400feb52780e69240a
olaysco <olayiwolaodunsi@gmail.com>
2021-02-12 05:09:15 +0100

    add auth0 jwt R256 authorizer

---
+++ backend/src/auth/authJwtSecret.ts
@@ -0,0 +1,30 @@
+import { Jwt } from "./Jwt";
+import Axios, { AxiosResponse } from "axios";
+import { createLogger } from "../utils/logger";
+
+export default async (jwksUrl: string, jwt: Jwt): Promise<any> => {
+    try {
+        const result: AxiosResponse = await Axios.get(jwksUrl)
+        const keys = result.data.keys;
+
+        const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
+                    && key.kty === 'RSA' // We are only supporting RSA
+                    && key.kid           // The `kid` must be present to be useful for later
+                    && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
+            ).map(key => {
+                return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+            });
+        
+        const signingKey = signingKeys.find(key => key.kid === jwt.header.kid);
+        if (!signingKey) {
+            throw new Error('signing key not found'); 
+        }
+        return signingKey;
+    } catch (error) {
+        createLogger('JWT').error(error);
+    }
+}
+
+// const getSigningKey = async (kid) => {
+    
+// }
